d78f5318134a596e1a524e1270b5939e207289f7
Rahandi <rahandinoor@gmail.com>
2018-08-23 02:26:30 +0700

    kurang nutup foreach

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -16,6 +16,7 @@
                         <div class="col col-md-6" data-label="name">{{ $d->nama }}</div>
                         <button class="col col-md-2 btnaction" data-label="detail">Detail</button>
                     </li>
+                @endforeach
                 {{--<li class="table-row">--}}
                     {{--<div class="col col-md-4" data-label="nrp">05111640000052</div>--}}
                     {{--<div class="col col-md-6" data-label="name">John Doe</div>--}}
