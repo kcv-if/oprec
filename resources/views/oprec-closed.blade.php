cd53e5af6db188b8f36aec6368cfec31cdf56e3b
unknown <yolandahertita903@gmail.com>
2018-09-02 15:52:09 +0700

    pendaftaran ditutup

---
+++ resources/views/oprec-closed.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@include('layouts.nav')
+@section('content')
+	<div class="containerwrap">
+        <img src="{{asset('images/ditutup.jpg')}}" alt style="width: 100%; height: calc(100% - 30px);">
+    </div>
+@endsection
